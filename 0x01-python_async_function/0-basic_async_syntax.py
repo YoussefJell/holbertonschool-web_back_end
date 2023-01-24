34519c0c9c3e130426ca27dfb5debdf65fa9cb1d
YoussefJell <youssef.jellouli@yahoo.com>
2023-01-24 17:50:51 +0000

    add

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 0-module """
+import random
+import asyncio
+
+async def wait_random(max_delay = 10):
+	""" Write an asynchronous coroutine that takes in an integer argument
+	and returns it """
+	delay = random.uniform(0,max_delay)
+	return delay

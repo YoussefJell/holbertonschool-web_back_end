60509b642e323abda8707855608614301ea2f3e5
YoussefJell <youssef.jellouli@yahoo.com>
2023-01-31 00:11:38 +0000

    add

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -51,6 +51,7 @@ class Server:
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """return a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a
+    """return a tuple of size two containing a start index and an
+        end index corresponding to the range of indexes to return in a
     list for those particular pagination parameters."""
     return (((page*page_size)-page_size, (page*page_size)))

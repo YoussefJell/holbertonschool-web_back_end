cc9a38ebd0c8f4ea63b83dcbaeba71a7eaba2d18
YoussefJell <youssef.jellouli@yahoo.com>
2023-02-07 11:48:19 +0000

    add

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -7,13 +7,13 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 AUTH_TYPE = getenv("AUTH_TYPE")
 auth = None
 if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
     auth = Auth()
 elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth

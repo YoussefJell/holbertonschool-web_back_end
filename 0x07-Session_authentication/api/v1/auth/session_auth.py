37c3f6b0caad102c50e687dab69259a5b1dd2d36
YoussefJell <youssef.jellouli@yahoo.com>
2023-02-09 14:26:41 +0100

    add

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,11 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ comment """
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return None
+        _id = self.user_id_for_session_id(session_cookie)
+        return User.get(_id)

7d5ad9241d56d0da3c004e7b8fc1bf810f076f71
YoussefJell <youssef.jellouli@yahoo.com>
2023-02-09 14:09:10 +0100

    add

---
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+auth module for the API
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ comment """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ comment """
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """ comment """
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ comment """
+        return None
---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+auth module for the API
+"""
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
+from uuid import uuid4
+
+class SessionAuth(Auth):
+    """ SessionAuth Class """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id == None or type(user_id) != str:
+            return None
+        sess_id = str(uuid4())
+        self.user_id_by_session_id[sess_id] = user_id
+        return sess_id

9921ecf76d11796646e8971b57c68e6ea3669971
YoussefJell <youssef.jellouli@yahoo.com>
2023-02-15 20:40:02 +0000

    add

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -10,11 +10,14 @@ class Auth:
     """
 
     def __init__(self):
+        """ comment init """
         self._db = DB()
 
     def register_user(self):
+        """ comment register """
         pass
 
 
 def _hash_password(password: str) -> str:
+    """ comment hash pass """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())

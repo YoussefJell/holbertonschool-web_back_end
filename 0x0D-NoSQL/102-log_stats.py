936880876038dc00b5fb25406b6c65f5377a2fd6
YoussefJell <youssef.jellouli@yahoo.com>
2023-03-13 15:11:54 +0000

    add

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -18,3 +18,23 @@ if __name__ == "__main__":
     status_check = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"})
     print(f'{status_check} status check')
+
+    ips = nginx_collection.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+         },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": 1,
+            "count": 1
+        }}
+    ])
+
+    print("IPs:")
+    for top_ips in ips:
+        print(f'\t{top_ips.get("ip")}: {top_ips.get("count")}')

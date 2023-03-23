2217d9120d71542002d34b158cff62b707e52a9e
YoussefJell <youssef.jellouli@yahoo.com>
2023-03-23 21:12:42 +0000

    add

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,16 +1,15 @@
 export default class Building {
-	constructor(sqft) {
-		if (
-		  this.constructor !== Building
-		  && typeof this.evacuationWarningMessage !== 'function'
-		) {
-		  throw new Error('Class extending Building must override\
- evacuationWarningMessage');
-		}
-		this._sqft = sqft;
-	  }
+  constructor(sqft) {
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
 
-	  get sqft() {
-		return this._sqft;
-	  }
+  get sqft() {
+    return this._sqft;
+  }
 }

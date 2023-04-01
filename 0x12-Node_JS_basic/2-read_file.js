8c73b31aa2a054ee3f5dc35f478656bbab130bd6
YoussefJell <youssef.jellouli@yahoo.com>
2023-04-01 22:40:58 +0000

    ad

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 
 module.exports = function countStudents(filename) {
-  fs.readFile(filename, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-    console.log(data.split('\n').filter((e) => e).length - 1);
-  });
-}
+  let content;
+  try {
+    content = fs.readFileSync(filename);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+  const studentArr = {};
+  content = content.toString().split('\n').filter((e) => e);
+  for (let i = 1; i < content.length; i += 1) {
+    const element = content[i].split(',');
+    if (!studentArr[element[3]]) studentArr[element[3]] = [];
+    studentArr[element[3]].push(element[0]);
+  }
+  console.log(`Number of students: ${content.length - 1}`);
+  for (const key of Object.keys(studentArr)) {
+    console.log(`Number of students in ${key}: ${studentArr[key].length}. List: ${studentArr[key].join(', ')}`);
+  }
+};
